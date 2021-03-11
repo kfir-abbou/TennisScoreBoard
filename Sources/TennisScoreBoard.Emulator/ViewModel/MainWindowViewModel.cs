71dacff84d69bd97f04a27788ca5b4fb4b023114
kfir-abbou <kfir.abbou@gmail.com>
2021-03-11 23:26:48 +0200

    Create applictation skeletone

---
+++ Sources/TennisScoreBoard.Emulator/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace TennisScoreBoard.Emulator.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
